c5eb524c764510a45a2e0665992868a12b6e56ad
JB Lewis <jblewis@gmail.com>
2017-09-14 15:22:37 -0500

    Completed Tests for Update- and Remove-SPListItem

--- Tests/HCSharepoint.Tests.ps1
+++ Tests/HCSharepoint.Tests.ps1
@@ -4,6 +4,7 @@ Import-Module .\HCSharepoint
 
 InModuleScope "HCSharepoint" {
     # Known info about the Sharepoint list we are testing against
+    # https://team.hennepin.us/VEX/lists/PrintFaxServices
     $URI = 'https://team.hennepin.us/VEX'
     $ListName = 'PrintFaxServices'
     $knownRecords = 78
@@ -53,10 +54,10 @@ InModuleScope "HCSharepoint" {
     }
     Describe "New-SPListItem" {
         $RecordObject = [pscustomobject]@{
-            Title = "Monad"
+            Title       = "Monad"
             MachineName = "Monad"
             ServiceName = "Automator"
-            Status = "hibernating"
+            Status      = "hibernating"
         }
         Context "Example 1: Create a new record by paramter" {
             New-SPListItem -uri $URI -listname $ListName -record $RecordObject
@@ -74,12 +75,89 @@ InModuleScope "HCSharepoint" {
             $RecordObject.Title = "Monad2"
             $RecordObject | New-SPListItem -uri $URI -listname $ListName
 
+            $ListItems = Get-SPListItem -uri $URI -listname $ListName 
             it "Now returns $($knownRecords + 2) Records" {
                 $ListItems.count | should be ($knownRecords + 2)
             }
             it "Title of the last record is 'Monad2'" {
                 $ListItems | Select -Last 1 -ExpandProperty Title | Should be $RecordObject.Title
             }
+        }
+    }
+    # These next tests assume that the previous ones passed
+    Describe "Update-SPListItem" {
+        $id = Get-SPListItem -uri $URI -listname $ListName | Select -Last 1 -ExpandProperty ID
+        Context "Example 1: Update one Field of one record" {
+            Update-SPListItem -uri $URI -listname $ListName -id $id -field MachineName -value "PowerShell"
+
+            $listItem = Get-SPListItem -uri $URI -listname $ListName | Select -Last 1
+
+            it "Changes the MachineName Property to 'PowerShell'" {
+                $listItem.MachineName | Should be 'PowerShell'
+            }
+        }
+        Context "Example 2: Update an item from a PSCustomObject" {
+            $update = [PSCustomObject]@{
+                ID     = $id
+                Status = "Awake"
+            }
+            Update-SPListItem -uri $uri -listname $ListName -fields $update
+
+            $listItem = Get-SPListItem -uri $URI -listname $ListName | Select -Last 1
+
+            it "Status is now 'Awake'" {
+                $listItem.Status | should be 'Awake'
+            }
+        }
+        Context "Example 3: Piping a PSCustomObject" {
+            $update = [PSCustomObject]@{
+                ID     = $id
+                Status = "Drowsy"
+            }
+            $update | Update-SPListItem -uri $uri -listname $ListName
+
+            $listItem = Get-SPListItem -uri $URI -listname $ListName | Select -Last 1
+
+            it "Status is now $($update.Status)" {
+                $listItem.Status | should be $update.Status
+            }
+        }
+        Context "Handling 1: Make an update with a wrong case column name" {
+            Update-SPListItem -uri $URI -listname $ListName -id $id -field status -value "Sleepy"
+
+            $listItem = Get-SPListItem -uri $URI -listname $ListName | Select -Last 1
+            
+            it "Status field correctly cased, and is now 'Sleepy'" {
+                $listItem.Status | should be 'Sleepy'
+            }
+        }
+        Context "Error 1: No ID property in the input object" {
+            $update = [PSCustomObject]@{
+                Title  = "Monad2"
+                Status = "Awake"
+            }
+
+            it "throws when it doesn't know what record to update" {
+                {Update-SPListItem -uri $uri -listname $ListName -fields $update } | Should Throw
+            }
+        }
+    }
+    Describe "Remove-SPListItem" {
+        
+        Context "Example 1: Remove item by ID" {
+            $id = Get-SPListItem -uri $URI -listname $ListName | Select -Last 1 -ExpandProperty ID
+            Remove-SPListItem -uri $URI -listname $ListName -id $id -Confirm:$false
+
+            It "reduced the count to $($knownRecords + 1)" {
+                (Get-SPListItem -uri $URI -listname $ListName).count | Should be ($knownRecords + 1)
+            }
+            $id = Get-SPListItem -uri $URI -listname $ListName | Select -Last 1 -ExpandProperty ID
+            Remove-SPListItem -uri $URI -listname $ListName -id $id -Confirm:$false
+
+            It "reduced the count to $knownRecords" {
+                (Get-SPListItem -uri $URI -listname $ListName).count | Should be $knownRecords
+            }
+
         }
     }
 }

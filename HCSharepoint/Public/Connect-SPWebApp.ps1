4f1c9a77145ef7038be44b625318ac70b252fd67
JB Lewis <jblewis@gmail.com>
2018-07-13 09:04:33 -0500

    fleshed out Connect-SPWepApp

--- HCSharepoint/Public/Connect-SPWebApp.ps1
+++ HCSharepoint/Public/Connect-SPWebApp.ps1
@@ -1,33 +1,102 @@
-Begin
+<#
+.SYNOPSIS
+    Establishes connection with the SharePoint WebApp
+.DESCRIPTION
+    Leverages Connect-PNPOnline to establish a connection with a Sharepoint WebApp prior to accessing any data
+    from that webapp
+.EXAMPLE
+    Connect-SPWebApp -URL "https://site.domain.local/webapp" -UseCurrentCredentials
+
+    Connects using WinAuth to the WebApp using the current credentials
+.EXAMPLE
+    Connect-SPWebApp -URL "https://site.domain.local/webapp" -credential domain\username
+
+    Connects using WinAuth to the WebApp using the provided credentials
+.EXAMPLE
+    Connect-SPWebApp -URL "https://site.domain.local/webapp" -credential domain\username -UseADFS
+    
+    Connects using ADFS to the WebApp using the provided credentials
+.INPUTS
+    String - URL
+    Credentials
+.OUTPUTS
+    SharePointPnP.PowerShell.Commands.Base.SPOnlineConnection
+.NOTES
+    General notes
+.COMPONENT
+    The component this cmdlet belongs to
+.ROLE
+    The role this cmdlet belongs to
+.FUNCTIONALITY
+    Connect to SharePoint
+#>
+function Connect-SPWebApp
+{
+    [CmdletBinding(DefaultParameterSetName = "CurrentCredentials")]
+    [Alias()]
+    [OutputType([SharePointPnP.PowerShell.Commands.Base.SPOnlineConnection])]
+    Param (
+        # Param1 help description
+        [Parameter(Mandatory = $true,
+            Position = 0,
+            ValueFromPipeline = $true)]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [Alias("URI")]
+        [String]
+        $URL,
+        
+        # Param2 help description
+        [Parameter(Mandatory= $true,
+            ParameterSetName = 'CurrentCredentials')]
+        [switch]
+        $UseCurrentCredentials,
+        
+        # Credential
+        [Parameter(Mandatory = $true,
+            ParameterSetName = 'Credentials')]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $Credential,
+        
+        # Use ADFS authentication with the provided credentials
+        [Parameter(ParameterSetName = 'Credentials')]
+        [Switch]
+        $UseADFS,
+
+        # Ignore SSL Errors - Use with Caution!
+        [Parameter(AttributeValues)]
+        [Switch]
+        $IgnoreSSLErrors
+    )
+    
+    begin
     {
-        # First we need to connect
-        try 
+    }
+    
+    process
+    {
+        # Parameter Sets
+        if ($pscmdlet.ParameterSetName = 'CurrentCredentials')
         {
-            # There's a chance I might have to deal with mulitple connections...
-            # Ensure we are connected to the WebApp specified by $URI
-            $Connection = Get-PnPConnection
-            if ($Connection.url -ne $uri)
-            {
-                if ($PSBoundParameters['Credential'])
-                {
-                    $ConnectParam = @{
-                        Credentials = $Credential
-                    }
-                }
-                else
-                {
-                    $ConnectParam = @{
-                        CurrentCredentials = $true
-                    }
-                }
-                
-                $Connection = Connect-PnPOnline -ReturnConnection -Url $uri -UseAdfs:$UseADFS -ErrorAction Stop
+            $ConnectParam = @{
+                URL = $URL
+                ReturnConnection = $true
+                CurrentCredentials = $true
             }
         }
-        catch 
-        {
-            # I might want to handle this differently in the future
-            
-            Throw $_
+        else {
+            $ConnectParam = @{
+                URL = $URL
+                ReturnConnection = $true
+                Credentials = $Credential
+            }
         }
+
+        Connect-PnPOnline @ConnectParam -UseAdfs:$UseADFS -IgnoreSslErrors:$IgnoreSSLErrors
     }
+    
+    end
+    {
+    }
+}

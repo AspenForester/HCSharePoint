4c177c044a4c26bde5f54b373a414183bcc760eb
JB Lewis <jblewis@gmail.com>
2018-07-16 07:53:50 -0500

    Adjusted to use Connect-SPWebApp

--- HCSharepoint/Public/Get-SPListField.ps1
+++ HCSharepoint/Public/Get-SPListField.ps1
@@ -52,20 +52,19 @@ Function Get-SPListField
             $Connection = Get-PnPConnection
             if ($Connection.url -ne $uri)
             {
-                if ($PSBoundParameters['Credential'])
-                {
-                    $ConnectParam = @{
-                        Credentials = $Credential
-                    }
+                $ConnectParam = @{
+                    URI = $uri
                 }
-                else
+                If ($PSBoundParameters['Credential'])
                 {
-                    $ConnectParam = @{
-                        CurrentCredentials = $true
-                    }
+                    $ConnectParam.Add('Credential',$Credential)
+
                 }
-                
-                $Connection = Connect-PnPOnline -ReturnConnection -Url $uri -UseAdfs:$UseADFS -ErrorAction Stop
+                if ($PSBoundParameters['UseADFS']) 
+                {
+                    $ConnectParam.Add('UseADFS',$UseADFS)
+                }
+                Connect-SPWebApp @ConnectParam -ErrorAction Stop
             }
         }
         catch 
@@ -91,9 +90,10 @@ Function Get-SPListField
             "_Level", "_IsCurrentVersion", "ItemChildCount", "FolderChildCount", "AppAuthor",
             "AppEditor", "Restricted")
 
+        # Connection information is held as a Script Scope variable!
         $Lists = Get-PnPList
         # Case Sensitive List Name
-        $CSList = ($Lists | Where Title -like $listname).Title
+        $CSList = ($Lists | Where-Object Title -like $listname).Title
        
         $ListFields = Get-PnPField -Identity -List $CSList 
 
